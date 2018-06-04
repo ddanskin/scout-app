ca496d03f27f1876abf62306220b05dc53f1c820
Danielle Danskin <danielle.danskin@gmail.com>
2018-06-03 22:27:20 -0400

    switch from semantic ui to bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_one :destination_rating
+    has_many :destination_ratings, dependent: :destroy
+    accepts_nested_attributes_for :destination_ratings
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
